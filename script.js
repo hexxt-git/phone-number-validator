8ab33fc337f80c28be8949ca8f9387f13e4e7c0c
9EED <zeghdns@gmail.com>
2022-04-28 07:38:19 +0100

    95% done

--- script.js
+++ script.js
@@ -16,15 +16,58 @@ function error(input){
 
 function telephoneCheck(str) {
     if(
-      /^1? ?[0-9]{3}\-[0-9]{3}\-[0-9]{4}$/.test(str)|
-      /^1*\([0-9]{3}\)[0-9]{3}\-[0-9]{4}$/.test(str)|
-      /^1 \([0-9]{3}\)[0-9]{3}\-[0-9]{4}$/.test(str)|
-      /^1 \([0-9]{3}\) [0-9]{3}\-[0-9]{4}$/.test(str)|
-      /^1* *[0-9]{3} [0-9]{3} [0-9]{4}$/.test(str)|
-      /^[0-9]{10}$/.test(str)
+		/^[0-9]{10}$/.test(str)|// 							  		1234567890
+		/^[0-9]+ [0-9]{10}$/.test(str)|//	 				  		69 1234567890
+    	/^[0-9]{3} [0-9]{3} [0-9]{4}$/.test(str)|//           		123 456 7890
+    	/^[0-9]+ [0-9]{3} [0-9]{3} [0-9]{4}$/.test(str)|//    		69 123 456 7890
+		/^[0-9]{3}\-[0-9]{3}\-[0-9]{4}$/.test(str)| //		  		123-456-7890
+		/^[0-9]+ [0-9]{3}\-[0-9]{3}\-[0-9]{4}$/.test(str)|//	  	69 123-456-7890
+      	/^\([0-9]{3}\)[0-9]{3}\-[0-9]{4}$/.test(str)|//    			(123)456-7890
+      	/^[0-9]+\([0-9]{3}\)[0-9]{3}\-[0-9]{4}$/.test(str)|//	 	69 (123)456-7890
+    	/^[0-9]+ \([0-9]{3}\)[0-9]{3}\-[0-9]{4}$/.test(str)//	 	69(123)456-7890
     ){
       return true
     }
-      return false
+    return false
   }
-//write(telephoneCheck(prompt('phone')));
+  let phone = []
+  let updatePhone = ()=>{
+    document.getElementById('phone').innerText = phone.join('')
+  }
+for ( let i = 0 ; i <= 12 ; i++){
+	document.getElementById(`n${i}`).addEventListener( 'click', ()=>{
+    	phone.push(document.getElementById(`n${i}`).innerText)
+    	updatePhone()
+ 	})
+}
+document.getElementById(`n13`).addEventListener( 'click', ()=>{
+  	phone.push(' ')
+  	updatePhone()
+})
+document.getElementById(`n14`).addEventListener( 'click', ()=>{
+ 	phone.pop()
+ 	updatePhone()
+})
+document.getElementById(`home`).addEventListener( 'click', ()=>{
+  	phone = []
+  	updatePhone()
+})
+document.getElementById('nC').addEventListener('click',()=>{
+  	if(telephoneCheck(phone.join(''))){
+		phone = []
+		document.getElementById('phone').innerHTML = `
+		<div class='green'>
+			valid phone number
+		</div>
+		`
+	}
+  	else{
+		phone = []
+		document.getElementById('phone').innerHTML = `
+		<div class='red'>
+			invalid phone number
+		</div>
+		`
+  }	
+})
+
